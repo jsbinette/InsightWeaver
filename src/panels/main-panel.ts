69e89e1a36a81c5388ae065bcf2345f204bcfca6
Jean-Sebastien Binette <jsbinette@yahoo.com>
2025-04-01 16:13:40 -0400

    added unsafe-inline styles

--- src/panels/main-panel.ts
+++ src/panels/main-panel.ts
@@ -173,7 +173,8 @@ export class ChatGptPanel {
           <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'self' 'unsafe-inline'; font-src ${webview.cspSource}; img-src ${webview.cspSource} data: blob: https:; script-src 'nonce-${nonce}'; sandbox allow-scripts allow-popups;"><link href="${styleVSCodeUri}" rel="stylesheet">
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'self' 'unsafe-inline'; font-src ${webview.cspSource}; img-src ${webview.cspSource} data: blob: https:; script-src 'nonce-${nonce}';">
+            <link href="${styleVSCodeUri}" rel="stylesheet">
             <link rel="icon" type="image/jpeg" href="${logoMainPath}">
           </head>
           <body>        
--- src/views/sidebar-view.ts
+++ src/views/sidebar-view.ts
@@ -85,7 +85,7 @@ export class SideBarViewProvider implements vscode.WebviewViewProvider {
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${styleVSCodeUri}" rel="stylesheet">			
 				<title>Panel</title>
